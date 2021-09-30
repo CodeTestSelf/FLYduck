8a17d9780e1fc42ffd88eb4e6cd1a098ff58d94d
bitlakrishnasai <krishnasai.bitla@gmail.com>
2021-09-30 21:05:32 +0530

    m

---
+++ mongo.py
@@ -0,0 +1,11 @@
+DatabaseName = "hi"
+
+showDarabases = "show dbs" #shows available databases
+
+createNewDatabase = "use " + DatabaseName #Creates new Database
+
+switchDatabase = "use "+ DatabaseName #Switches to database
+
+
+
+print(createNewDatabase)
