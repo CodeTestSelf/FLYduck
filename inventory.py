a0bda484d34aae33d39f42b2852d22209faeec94
bitlakrishnasai <krishnasai.bitla@gmail.com>
2021-10-01 15:48:08 +0530

    inventory

--- inventory.py
+++ inventory.py
@@ -1,14 +1,26 @@
 
 
-def addInventory(serialNum,productName,grossWeight,netWeight):
-    inventoryVariables = {"SerialNum"       : "", 
-                          "ProductName"     : "", 
-                          "grossWeight"     : "" , 
-                          "netWeight"       : "", 
+def addInventory(inventoryVariables):
+    inventoryVariables = {"SerialNum"       : inventoryVariables["SerialNum"], 
+                          "ProductName"     : inventoryVariables["ProductName"], 
+                          "GrossWeight"     : inventoryVariables["GrossWeight"] , 
+                          "NetWeight"       : inventoryVariables["NetWeight"], 
+                          "Material"        : inventoryVariables["Material"], 
+                          "Purity"          : inventoryVariables["Purity"],
+                          "Description"     : inventoryVariables["Description"],
+                          "PurchasedFrom"   : inventoryVariables["PurchasedFrom"],
+                          "PurchaseBillNum" : inventoryVariables["PurchaseBillNum"] 
+                      }
+    print(inventoryVariables)
+
+inventoryVariables = {"SerialNum"       : "12345", 
+                          "ProductName"     : "ring", 
+                          "GrossWeight"     : 1 , 
+                          "NetWeight"       : "", 
                           "Material"        : "", 
                           "Purity"          : "",
                           "Description"     : "",
                           "PurchasedFrom"   : "",
                           "PurchaseBillNum" : "" 
                       }
-    print("hi")
+addInventory(inventoryVariables)
