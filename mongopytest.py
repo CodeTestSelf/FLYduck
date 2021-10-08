8996b7b0451082e7680cabdfd9386dd83102b5b3
bitlakrishnasai <krishnasai.bitla@gmail.com>
2021-10-08 20:38:27 +0530

    serial key

--- mongopytest.py
+++ mongopytest.py
@@ -2,7 +2,35 @@ from mongo import searchOne
 import pymongo
 
 
-            
+def serialKey(productName):
+
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myclient[databaseName]
+    mycol = mydb[collectionName]
+    
+    counter=1
+    for y in mycol.find():
+        z=[*y.keys()]
+        break
+    
+    print(len(z))
+
+    
+    mydoc = mycol.find().sort("_id")
+    for x in mydoc:
+        if x["_id"]==counter:
+            counter+=1
+        else:
+            print(counter)
+            return counter
+
+    print(mydoc)
+
+    #x= mycol.insert_many(mydoc1)
+        
+
+
+
 
 databaseName="SoumyaJewellers"
 collectionName="Inventory"
@@ -10,3 +38,4 @@ mydict1=int(12345)
 
 
 
+serialKey(1)
