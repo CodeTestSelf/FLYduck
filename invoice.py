21ed56c399adf7efaa07b54021bdfbc884b2635a
bitlakrishnasai <krishnasai.bitla@gmail.com>
2021-10-09 17:31:59 +0530

    invoice

--- invoice.py
+++ invoice.py
@@ -4,24 +4,27 @@ from reportlab.lib.pagesizes import letter
 
 def generate_invoice():
     c = canvas.Canvas("sample.pdf", pagesize=letter)
-    c.drawString(270, 800, "Invoice")
+
+
+    c.drawImage("bill.jpg", 0, 0,width=600,height=800)
+    # c.drawString(0, 800, "Invoice")
     
 
-    c.setLineWidth(.3)
-    c.setFont('Helvetica', 12)
+    # c.setLineWidth(.3)
+    # c.setFont('Helvetica', 12)
 
-    c.drawString(30, 750, 'OFFICIAL COMMUNIQUE')
-    c.drawString(30, 735, 'OF ACME INDUSTRIES')
-    c.drawString(500, 750, "12/12/2010")
-    c.line(480, 747, 580, 747)
+    # c.drawString(30, 750, 'OFFICIAL COMMUNIQUE')
+    # c.drawString(30, 735, 'OF ACME INDUSTRIES')
+    # c.drawString(500, 750, "12/12/2010")
+    # c.line(480, 747, 580, 747)
 
-    c.drawString(275, 725, 'AMOUNT OWED:')
-    c.drawString(500, 725, "$1,000.00")
-    c.line(378, 723, 580, 723)
+    # c.drawString(275, 725, 'AMOUNT OWED:')
+    # c.drawString(500, 725, "$1,000.00")
+    # c.line(378, 723, 580, 723)
 
-    c.drawString(30, 703, 'RECEIVED BY:')
-    c.line(120, 700, 580, 700)
-    c.drawString(120, 703, "JOHN DOE")
+    # c.drawString(30, 703, 'RECEIVED BY:')
+    # c.line(120, 700, 580, 700)
+    # c.drawString(120, 703, "JOHN DOE")
 
 
 
